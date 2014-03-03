60941bcef78f824fb484545412dd6d7e9eb0e96c
Tammie <Tammie@Tammie-Envy>
2014-03-03 16:20:26 -0600

    added methods and properties to Customer and Receipt

--- src/trhpos/Receipt.java
+++ src/trhpos/Receipt.java
@@ -3,4 +3,26 @@ package trhpos;
 
 public class Receipt {
     //has a customer, line item array
+    private Customer customer;
+    private LineItem[] lineItemArray;
+    private static int receiptNo = 0;
+    private DataStorageStrategy db = new DatabaseStorage();
+    
+        public Receipt(String customerId) {
+        this.customer = findCustomer(customerId);
+        receiptNo++;
+        lineItemArray = new LineItem[0];
+    }
+
+    private Customer findCustomer(String customerId) {
+        return db.findCustomer(customerId);
+    }
+        public void addLineItem(String prodId, int qty) {
+        LineItem item = new LineItem(prodId, qty);
+        addToArray(item);
+    }
+
+    private void addToArray(LineItem item) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
