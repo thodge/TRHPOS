88e49e21568efb9879b557b0d38e5befaeaa59fa
Tammie <Tammie@Tammie-Envy>
2014-03-03 16:31:46 -0600

    added OutputStrategy

---
+++ src/trhpos/OuputReceiptToConsole.java
@@ -0,0 +1,12 @@
+package trhpos;
+
+public class OuputReceiptToConsole implements OutputReceiptStrategy {
+
+    @Override
+    public void outputReceipt(Customer customer, LineItem[] lineItems) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    
+}
