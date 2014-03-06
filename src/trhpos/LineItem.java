337f6366ec683b2417f2b98040d41977a9f08926
Tammie <Tammie@10.30.19.166>
2014-03-05 20:22:10 -0600

    Fixed LineItem and Receipt

--- src/trhpos/LineItem.java
+++ src/trhpos/LineItem.java
@@ -1,17 +1,45 @@
 package trhpos;
 
-
 public class LineItem {
-    //has product
-    private DiscountStrategy discount;
-    private Product product;
-    private String[] lineItemArray;
 
-    LineItem(String prodId, int qty) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    //has product
+    private DataStorageStrategy db;
+    private Product product;
+    private int qty;
+
+    public LineItem(DataStorageStrategy db, String productId, int qty) {
+        setDb(db);
+        this.product = findProduct(productId);
+        setQty(qty);
     }
-    
-    
-    
-    
+
+    private final Product findProduct(final String productId) {
+        // validation needed
+        return db.findProduct(productId);
+    }
+
+    public DataStorageStrategy getDb() {
+        return db;
+    }
+
+    public void setDb(DataStorageStrategy db) {
+        this.db = db;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
 }
